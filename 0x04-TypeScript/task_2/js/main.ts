652d134955cebb51ee35712cd72941838322a61b
Joshua Kyalo <56042440+Yusha254@users.noreply.github.com>
2025-09-14 20:28:45 +0300

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -39,11 +39,12 @@ class Teacher implements TeacherInterface {
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
+  if (salary < 500) {
     return new Teacher();
   }
   return new Director();
 }
+
 
 function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
